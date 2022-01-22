6cab2cf0560aa836cad591cbc5ad88ca42f4c701
Yoann Diep <yoann.diep@hotmail.fr>
2022-01-22 10:41:09 +0100

    Modified the script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const algoliaPlacesApiAppId = 'plU4N8HG6QWK';
 const algoliaPlacesApiKey = '1131438afb49f60a48ed468c5af189b8';
 const mapboxApiToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
-const taxiFareApiUrl = 'http://localhost:8000/predict';
+const taxiFareApiUrl = 'https://taxifare-uvj5vais2q-ew.a.run.app/predict';
 
 const displayMap = (start, stop) => {
   mapboxgl.accessToken = mapboxApiToken;
@@ -206,7 +206,7 @@ const predict = () => {
       .then(data => {
         document.getElementById('fare').classList.remove('d-none');
         const fareResult = document.getElementById('predicted-fare');
-        const fare = Math.round(data['fare'] * 100) / 100
+        const fare = Math.round(data['prediction'] * 100) / 100
         fareResult.innerText = `$${fare}`;
       })
       .catch((error) => {
